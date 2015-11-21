d07d378d8fe80c4405011866d0788834489c8d8f
iMyon <myon.cn@gmail.com>
2015-11-22 00:09:49 +0800

    update data

--- mergeVote.js
+++ mergeVote.js
@@ -14,6 +14,7 @@ files.forEach(function(item) {
   var specialDay = [
   {date: "15-11-16",count: 8000},
   {date: "15-11-17",count: 8000},
+  {date: "15-11-22",count: 12000},
   {date: "15-11-18",count: 8000}
   ];
   var sumVote = 10000;  //初始一万票
